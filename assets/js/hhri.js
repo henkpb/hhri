c61230aadad48859efc6ccd0224d962a81d68642
Henk <henk@hermana.no>
2026-02-08 14:43:54 +0100

    Update main menu

--- assets/js/hhri.js
+++ assets/js/hhri.js
@@ -6,6 +6,20 @@
     const isDark = document.body.classList.contains('dark');
     document.body.classList.add(isDark ? 'light' : 'dark');
     document.body.classList.remove(isDark ? 'dark' : 'light');
+
+    try {
+      window.localStorage.setItem('darkmode', isDark ? '0' : '1');
+    }
+    catch (e) {}
   });
+
+  try {
+    const isDark = window.localStorage.getItem('darkmode') === '1';
+    if (document.body.classList.contains(isDark ? 'light' : 'dark')) {
+        document.body.classList.remove(isDark ? 'light' : 'dark');
+        document.body.classList.add(isDark ? 'dark' : 'light');
+    }
+  }
+  catch (e) {}
 
 })();
