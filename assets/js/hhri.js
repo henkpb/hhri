086041498eb2a89bfe99597a9b9e83f387bb347a
Henk <henk@hermana.no>
2026-02-09 18:12:39 +0100

    Update search

--- assets/js/hhri.js
+++ assets/js/hhri.js
@@ -22,4 +22,13 @@
   }
   catch (e) {}
 
+  var sjs = SimpleJekyllSearch({
+    searchInput: document.getElementById('search-input'),
+    resultsContainer: document.getElementById('results-container'),
+    json: '{{ "/search.json" | relative_url }}',
+    searchResultTemplate: '<li><a href="{url}">{title}</a></li>',
+    noResultsText: 'No results found',
+    limit: 10,
+    fuzzy: false
+  });
 })();
